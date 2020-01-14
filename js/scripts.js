fae93f8ee7b61b7a1fcd1ffbc35624f65673eeca
Erik Grove and Krista Rutz <erik@>
2020-01-14 15:21:04 -0800

    improve scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(".clickable").click(function() {
-        $("#walrus-showing").slideToggle();
-        $("#walrus-hidden").slideToggle();
+        $("#walrus-showing").toggle();
+        $("#walrus-hidden").toggle();
     });
 });
